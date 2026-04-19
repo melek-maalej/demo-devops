f0a435f5aadfdd88e6be14dcc30cecc2c2685ea9
melek4000 <melek2maalej@gmail.com>
2026-04-19 19:11:49 +0100

    empty project

---
+++ src/main/java/com/example/demo_devops/DemoDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDevopsApplication.class, args);
+	}
+
+}
